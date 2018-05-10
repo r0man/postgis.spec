d87e1159e6ab0118db8424b829d82ae2033cd121
r0man <roman@burningswell.com>
2018-05-10 16:26:30 +0200

    Version 0.1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject postgis.spec "0.1.3-SNAPSHOT"
+(defproject postgis.spec "0.1.3"
   :description "Clojure specs and generators for PostGIS types"
   :url "https://github.com/r0man/postgis.spec"
   :license {:name "Eclipse Public License"
