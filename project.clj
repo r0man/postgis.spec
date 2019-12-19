216298de7f84d0ed584209d4ef9a57bc77f60e71
r0man <roman@burningswell.com>
2019-12-19 23:15:30 +0100

    Update deps

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :deploy-repositories [["releases" :clojars]]
-  :dependencies [[org.clojure/clojure "1.9.0-RC2"]]
-  :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]
-                   :plugins [[jonase/eastwood "0.2.3"]
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :profiles {:dev {:dependencies [[org.clojure/test.check "0.10.0"]]
+                   :plugins [[jonase/eastwood "0.3.6"]
                              [lein-difftest "2.0.0"]
                              [listora/whitespace-linter "0.1.0"]]}
-             :provided {:dependencies [[net.postgis/postgis-jdbc "2.2.1"
+             :provided {:dependencies [[net.postgis/postgis-jdbc "2.3.0"
                                         :exclusions [org.postgresql/postgresql]]]}}
   :aliases {"ci" ["do" ["clean"] ["difftest"] ["lint"]]
             "lint" ["do"  ["eastwood"]]})
