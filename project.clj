0e6eddba9a703cf447c39af17e0c0c7b729b4ad9
Roman Scherer <roman@burningswell.com>
2024-05-07 20:49:54 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -5,11 +5,11 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :deploy-repositories [["releases" :clojars]]
   :dependencies [[org.clojure/clojure "1.10.2"]]
-  :profiles {:dev {:dependencies [[org.clojure/test.check "1.1.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/test.check "1.1.1"]]
                    :plugins [[jonase/eastwood "0.3.13"]
                              [lein-difftest "2.0.0"]
                              [listora/whitespace-linter "0.1.0"]]}
-             :provided {:dependencies [[net.postgis/postgis-jdbc "2.5.0"
+             :provided {:dependencies [[net.postgis/postgis-jdbc "2023.1.0"
                                         :exclusions [org.postgresql/postgresql]]]}}
   :aliases {"ci" ["do" ["clean"] ["difftest"] ["lint"]]
             "lint" ["do"  ["eastwood"]]})
