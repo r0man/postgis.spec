8815046cdde35d6b52062cb737d8da8e3a68c2d0
Roman Scherer <roman@burningswell.com>
2024-05-07 21:08:02 +0200

    Version 0.2.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject postgis.spec "0.2.0"
+(defproject postgis.spec "0.2.1-SNAPSHOT"
   :description "Clojure specs and generators for PostGIS types"
   :url "https://github.com/r0man/postgis.spec"
   :license {:name "Eclipse Public License"
